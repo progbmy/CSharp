7aa200571a06879572816b1a471c0420abc10342
MIhail <skuter63@mail.ru>
2021-06-13 01:56:24 +0400

    LEsson 29-30

---
+++ Lesson29/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson29
+{
+    /*
+     * Многомерные массивы
+     * Прямоуголые
+     * Зубчатые
+     * Ступенчатые
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lesson30/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[3, 5] 
+            {
+                { 23, 45,643,234,23},
+                { 65566, 5656,6345,2345,645},
+                { 822, 1661, 56511, 456465, 5334}
+            }; 
+            
+            
+            // Объявляем двумерный массив
+
+           // myArray[0, 2] = 99; // Помещаем значение в ячейку массива
+            
+            Console.WriteLine(myArray[0, 2]); // Считывем значение ячейки массива
+            
+
+        }
+    }
+}
