ed0cf89715bf2198496451f268b15f92682962d4
MIhail <skuter63@mail.ru>
2021-06-20 10:49:13 +0400

    lesson37

---
+++ Lesson37/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lesson37
+{
+    /*
+     * Область видимости
+     * 
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+        }
+    }
+}
