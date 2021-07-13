aaa38c18abd22ee4d5de5f9a6faf35d22979d591
MIhail <skuter63@mail.ru>
2021-07-14 00:09:51 +0400

    Lesson45

---
+++ Lesson45/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson45
+{ /*
+   * Параметр in передаваемый как аргумент
+   * 
+   */
+    class Program
+    {
+        static void Foo(in int value)
+        {
+            Console.WriteLine(value);
+            //value = 5;
+        }
+    
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Foo(a);
+        }
+    }
+}
