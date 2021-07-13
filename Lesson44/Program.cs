7ab507cb694c379cc4f721ff6da7d35681f8fce3
MIhail <skuter63@mail.ru>
2021-07-13 22:56:43 +0400

    Lesson 44

---
+++ Lesson44/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson44
+{ /*
+   * Ключевое слово Out 
+   * Разница между out и ref
+   */
+
+    class Program
+    {
+        static void Foo(ref int value)
+        {
+            value++;
+            Console.WriteLine(value);
+        }
+
+        static void Bar(out int value)
+        {
+            value = 0;
+            Console.WriteLine(value);
+        }
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Foo(ref a);
+            //Bar(out a);
+
+        }
+    }
+}
