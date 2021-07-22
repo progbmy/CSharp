b51466a53c6b7d436ec1f0a06febce6da1e692ed
MIhail <skuter63@mail.ru>
2021-07-22 22:45:52 +0400

    ls46-47

---
+++ Lesson46/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lesson46
+{
+    class Program
+    {
+        /*
+         * Ключевое слово params
+         
+
+        static int Sum(params int[] parameters)
+        {
+            int result = 0;
+            for (int i = 0; i < parameters.Length; i++)
+            {
+                result += parameters[i];
+            }
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int result = Sum(5, 10);
+            Console.WriteLine(result);
+        }
+        */
+
+        static void Foo(params object[] parameters)
+        {
+            string message = "Тип данных {0}, значение переменной {1}";
+            foreach (var item in parameters)
+            {
+                Console.WriteLine(message, item.GetType(), item);
+            }
+        
+        }
+
+        static void Main(string[] args)
+        {
+            Foo("test", 4, 'g', 5.56f, true);
+        
+        }
+    }
+}
---
+++ Lesson47/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson47
+{
+    /*
+     * Необязательные параметры методов
+     */
+    class Program
+    {
+        static int Sum(int a, int b, bool enableLogging = false)
+        { 
+            int result = a+ b;
+
+            if (enableLogging)
+            {
+                Console.WriteLine("Значение переменной a = " + a);
+                Console.WriteLine("Значение переменной b = " + b);
+                Console.WriteLine("Результат сложения = " + result);
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            int result = Sum(2, 5);
+            Console.WriteLine(result);
+
+        }
+    }
+}
