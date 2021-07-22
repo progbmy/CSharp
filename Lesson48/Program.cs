ae2a5e611b7c577702a09415545d02f8d6a43edb
MIhail <skuter63@mail.ru>
2021-07-22 23:35:58 +0400

    ls48

---
+++ Lesson48/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson48
+{
+    /*
+     * Именованные параметры
+     */
+    class Program
+    {
+        static int Sum(int a, int b, bool enableLogging =false)
+        {
+            int result = a + b;
+            if (enableLogging)
+            {
+                Console.WriteLine("Занчение переменной а= " + a);
+                Console.WriteLine("Значение переменной b = "+ b);
+                Console.WriteLine("Результат сложения = " + result);
+            }
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int result = Sum(b: 2, a: 6, enableLogging: true);
+        }
+    }
+}
