e440b9f8068c6cc67bfaafedea975e1ceba65288
MIhail <skuter63@mail.ru>
2021-08-01 14:26:57 +0400

    ls49

---
+++ Lesson49/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson49
+{
+
+    /*
+     * Рекурсия -вызов методом самого себя.
+     * 
+     * Методы и стек
+     * 
+     * 
+     * Переполнение стека
+     */
+
+    
+    class Program
+    {
+        static void Foo(int i)
+        {
+            Console.WriteLine(i);
+            i++;
+            Foo(i);
+
+        }
+        static void Main(string[] args)
+        {
+            Foo(0);
+        }
+    }
+}
