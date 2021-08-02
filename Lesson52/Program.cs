a76914e2b8e005da95e4428d876e7fb1d52a4025
MIhail <skuter63@mail.ru>
2021-08-02 21:16:24 +0400

    ls52-53

---
+++ Lesson52/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson52
+{
+    // Null - совместимые значимые типы (Nullable)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Lesson53/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson53
+{
+    /*
+     * Ключевое слово var 
+     * неявно типизированные переменные 
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var t = new float[10];
+            // Console.WriteLine(t.GetType());
+            var t = new { Name = "Мартин", Age = 34 };
+            Console.WriteLine(t.GetType());
+        }
+    }
+}
