64a3dfc10c39865e65ff8dd26891c18eb8a2bd5b
MIhail <skuter63@mail.ru>
2021-08-02 20:00:29 +0400

    ls50-51

---
+++ LEsson51/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LEsson51
+{
+    /*
+     * Переполнение по верхней и нижней границе
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte aggression = 0;
+            byte democracyModifire = 1;
+            aggression = checked((byte)(aggression - democracyModifire));
+            Console.WriteLine(aggression);
+        }
+    }
+}
---
+++ Lesson50/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson50
+{
+    /*
+     * Преобразование и приведение типов
+     * Неявное преобразование implicit
+     * Явное преобразование explicit double a =5.3; float b = (float)a
+     * 
+     */
+    class Program
+    {
+        static void Foo(float value)
+        {
+            Console.WriteLine(value);
+        }
+        static void Main(string[] args)
+        {
+            int a = 5;
+
+            Foo(a);
+        }
+    }
+}
