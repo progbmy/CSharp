106a7ff0568c13dfe79343cbfc2cc10b1f9f83d2
MIhail <skuter63@mail.ru>
2021-08-09 01:10:12 +0400

    ls56

---
+++ Lesson56/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lesson56
+{
+    class Program
+    {
+        
+       static Student GetStudent()
+        {
+            var student = new Student();
+            student.firstname = "Мартин";
+            student.lastname = "Игоревич";
+            student.middlename = "Рубинштейн";
+            student.age = 38;
+            student.id = Guid.NewGuid(); // Автогенерация id
+            student.group = "Лихтенгельн1";
+            return student;
+        }
+
+        static void Main(string[] args)
+        {
+            var firstStudent = GetStudent();
+            // firstStudent.Print();
+            string fullName = firstStudent.GetFullName();
+
+        }
+    }
+}
---
+++ Lesson56/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson56
+{
+    class Student
+    {
+        public Guid id;
+        public string firstname;
+        public string lastname;
+        public string middlename;
+        public int age;
+        public string group;
+
+        public void Print()
+        {
+            Console.WriteLine("Информация о студенте");
+            Console.WriteLine($"id: {id}");
+            Console.WriteLine($"Фамилия: {lastname}");
+            Console.WriteLine($"Имя: {firstname}");
+            Console.WriteLine($"Отчество: {middlename}");
+            Console.WriteLine($"Группа: {group}");
+        }
+        public string GetFullName()
+        {
+            return $"{lastname} {firstname} {middlename}";
+        }
+    }
+}
