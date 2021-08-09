5af00f50f021e6d1d0c71de4753a5d6ecf5d8835
MIhail <skuter63@mail.ru>
2021-08-09 23:33:04 +0400

    ls58

---
+++ Lesson58/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lesson58
+{
+    class Gun
+    {
+        private bool isLoaded;
+
+        private void Reload()
+        {
+            Console.WriteLine("Заряжаю...");
+            isLoaded = true;
+            Console.WriteLine("Заряжен...");
+        }
+
+        public void Shoot()
+        {
+            if (!isLoaded)
+            {
+                Console.WriteLine("Орудие не заряжено!");
+                Reload();
+
+            }
+            Console.WriteLine("Стреляем\n");
+            isLoaded = false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gun gun = new Gun();
+            gun.Shoot();
+        }
+    }
+}
