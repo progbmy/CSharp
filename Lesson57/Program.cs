f3dbaba3ef8bfd5b4d6b0b667cec0c4a96e6d9f5
MIhail <skuter63@mail.ru>
2021-08-09 22:56:56 +0400

    ls57

---
+++ Lesson57/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+
+namespace Lesson57
+{
+    /*
+     * Модификаторы доступа public private
+     */
+    class Point
+    {
+        public int x = 88;
+
+        private int y = 55;
+
+        private void PrintX()
+        {
+            Console.WriteLine($"X: {x}");
+        }
+        public void PrintY()
+        {
+            Console.WriteLine($"Y: {y}");
+        }
+
+        public void PrintPoint()
+        {
+            PrintX();
+            PrintY();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+            point.PrintY();
+            point.PrintPoint();
+        }
+    }
+}
