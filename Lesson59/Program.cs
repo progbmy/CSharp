2a87098e4f75f5d9538973869da7ba330a475ed3
MIhail <skuter63@mail.ru>
2021-08-23 22:14:52 +0400

    ls59

---
+++ Lesson59/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lesson59
+{
+    class Program
+    {
+        class Gun
+        {
+            public Gun(bool isLoaded)
+            {
+                _isLoaded = isLoaded;
+            }
+
+            private bool _isLoaded;
+            private void Reload()
+            {
+                Console.WriteLine("Заряжаю...");
+                _isLoaded = true;
+                Console.WriteLine("Заряжено...");
+            }
+            public void Shoot()
+            {
+                if (!_isLoaded)
+                {
+                    Console.WriteLine("Орудие заряжено!");
+                    Reload();
+                }
+                Console.WriteLine("Бах-Бах\n");
+                _isLoaded = false;
+            }
+        }
+            
+            
+        static void Main(string[] args)
+        {
+            Gun gun = new Gun(isLoaded: true);
+            gun.Shoot();
+        }
+    }
+}
